7ee9af7059e22291b71c8489ab7146a673dcc391
konki76 <mat_planchot@hotmail.fr>
2018-12-21 21:39:35 +0100

    suppression du constructeur

--- php/Cours.php
+++ php/Cours.php
@@ -5,11 +5,7 @@ abstract class Cours{
     protected $ageMini;
     protected $lesJours;  // collection des jours où a lieu le cours
 
-    public function __construct($unId,$unAgeMini){   // constructeur
-        $this->id=$unId;
-        $this->ageMini=$unAgeMini;
-        $this->lesJours=array();
-    }
+    //  j'ai enlevé le contructeur
 
 	abstract protected function Afficher();
 
